c4d501c0055ea3a31a8b3d19c05aecf1706a5b46
Brandon Harris <brandon@relentnet.com>
2024-11-23 23:16:03 -0600

    Added Zustand Middleware for Local Storage access.

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useStore = create((set) => ({
-    count: 0,
-    increment: () => set((state) => ({ count: state.count + 1 })),
-    decrement: () => set((state) => ({ count: state.count - 1 })),
-    reset: () => set({ count: 0 }),
-}));
+const useStore = create(
+    persist(
+        (set) => ({
+            count: 0,
+            increment: () => set((state) => ({ count: state.count + 1 })),
+            decrement: () => set((state) => ({ count: state.count - 1 })),
+            reset: () => set({ count: 0 }),
+        }),
+        { name: 'counter-storage' }
+    )
+);
 
 export default useStore;
